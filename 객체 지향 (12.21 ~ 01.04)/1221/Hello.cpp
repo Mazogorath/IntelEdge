d1be4ca37a73a0b09abd3fd3856b6c0624e52c0c
Mazogorath <136434148+Mazogorath@users.noreply.github.com>
2023-12-21 15:03:11 +0900

    Update Hello.cpp

--- 객체 지향 (12.21 ~ 01.04)/1221/Hello.cpp
+++ 객체 지향 (12.21 ~ 01.04)/1221/Hello.cpp
@@ -1,15 +1,6 @@
 #include <iostream>
-#include <string>
 
 int main(void) {
-	char rom_rock[10];
-	char jul_rock[10];
-	std::cout << "로미오>>";
-	std::cin >> rom_rock;
-
-	std::cout << "줄리엣>>";
-	std::cin >> jul_rock;
-
-	if (rom_rock == "바위" && jul_rock == "가위") { std::cout << "로미오가 이겼습니다"; }
+	std::cout << "Hello Word!";
 	return 0;
 }
